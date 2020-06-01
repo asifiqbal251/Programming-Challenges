295809642dcb60bbca8fc4e2d123c91e521a922b
asifiqbal251 <asif.iqbal@mail.mcgill.ca>
2020-06-01 11:27:45 -0600

    Python code for uniqueEmailAddress with doctest

--- LeetCode/929UniqueEmailAddress.py
+++ LeetCode/929UniqueEmailAddress.py
@@ -1,23 +1,29 @@
 def numUniqueEmails(emails):
     """ (list) -> int
-    returns 
+    Returns the number of unique and correctly formatted emails from a list of emails
+    >>> numUniqueEmails(["test.email+alex@leetcode.com", "test.email@leetcode.com"])
+    1
+    >>> numUniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com", \
+    "testemail+david@lee.tcode.com", "testemail+da@lee.tcode.com"])
+    2
     """
     L = len(emails)
+    
     for i in range(L):
-        # split the email on '+' and only take the first split
-        local_before_plus = emails[i].split('+')[0]
-        # remove every . in local address
-        local_without_dot = local_before_plus.replace('.','')
-        # split the local on '@' and only take the first split as the local may contain '@'
-        local_without_dot_at = local_without_dot.split('@')[0]
+        # split the  email on '@' and retrieve the domain
         domain = emails[i].split('@')[1]
-        emails[i] = local_without_dot_at+'@'+domain
+        # split the email on '+' and only take the split before '+' (the first split)
+        local_before_plus = emails[i].split('+')[0]
+        # remove every '.' in the local address
+        local_without_dot = local_before_plus.replace('.','')
+        # split the local on '@' and only take the first split as the local may contain '@' if '+' doesn't exist
+        local_without_dot_and_at = local_without_dot.split('@')[0]
+        # construct the email address
+        emails[i] = local_without_dot_and_at+'@'+domain
     return len(set(emails))
-            
-lis=["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com",\
-     "testemail+david@lee.tcode.com"]
 
-print(lis)
-print(numUniqueEmails(lis))
+if __name__=='__main__':
+    import doctest
+    doctest.testmod()       
         
         
